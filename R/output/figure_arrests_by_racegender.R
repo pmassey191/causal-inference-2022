c82f59cb2c8e9251e100ef68897c2b270b9dbe39
pmassey191 <pmassey@utexas.edu>
2022-02-15 23:10:49 -0600

    a

--- R/output/figure_arrests_by_racegender.R
+++ R/output/figure_arrests_by_racegender.R
@@ -1,17 +1,20 @@
 # Builds a bar graph with total arrests on the y axis and race/gender on the
 # x axis. Refer to my presentation on graphing for more detail.
-
-read_csv(here("data/NLSY97_clean.csv")) %>%
+library(ggthemes)
+  read_csv(here("data/NLSY97_clean.csv")) %>%
   group_by(race, gender) %>%
-  summarize(incarceration_rate = sum(incarcerated)/n()) %>%
-  ggplot(aes(race, months_incarcerated, fill = gender)) +
+  summarize(count = n(),
+            inc = sum(incarcerated),
+            inc_rate = inc/count) %>%  
+  ggplot(aes(race, inc_rate, fill = gender)) +
     geom_bar(stat = "identity", position = "dodge") +
     labs(
       x = "Race", 
-      y = "Mean Months Incarcerated", 
+      y = "Incarceration Rate", 
       fill = "Gender",
-      title = "Mean Number of Arrests in 2002 by Race and Gender") +
-    theme_minimal() +
-    scale_fill_economist()
+      title = "Incarceration Rate in 2002 by Race and Gender") +
+    theme_fivethirtyeight() +
+    scale_fill_economist()+
+    scale_y_continuous(labels = scales::percent)
 
-ggsave(here("figures/arrests_by_racegender.png"), width=8, height=4.5)
+ggsave(here("figures/incarceration_rate_by_racegender.png"), width=8, height=4.5)
